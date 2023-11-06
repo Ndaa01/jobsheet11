0a6c6578aa6df07a9322dba5b77bb289c919c4e0
Ndaa01 <ananda.idn12345@gmail.com>
2023-11-06 12:39:27 +0700

    Pertanyaan 1

--- Star05.java
+++ Star05.java
@@ -5,7 +5,7 @@ public class Star05 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Masukan Nilai N = ");
         int N = sc.nextInt();
-        for (int i = 0; i <= N; i++) {
+        for (int i = 1; i <= N; i++) {
             System.out.print("*");
         }
     }
