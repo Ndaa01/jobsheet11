8ad2c43cc7ead4547c269a0158db44be6a11457c
Ndaa01 <ananda.idn12345@gmail.com>
2023-11-06 14:23:05 +0700

    Pertanyaan 3

--- NestedLoop_2341720132.java
+++ NestedLoop_2341720132.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class NestedLoop_2341720132 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double [][] temps = new double[5][7];
+        double[][] temps = new double[5][7];
 
         for (int i = 0; i < temps.length; i++) {
             System.out.println("Kota Ke-" + i);
@@ -13,11 +13,16 @@ public class NestedLoop_2341720132 {
             }
             System.out.println();
         }
+        
         for (int i = 0; i < temps.length; i++) {
             System.out.println("Kota Ke-" + (i + 1) + ": ");
-            for (int j = 0; j < temps[0].length; j++) {
-                System.out.println(temps[i][j] + " ");
+            double total = 0;
+            for (double temp : temps[i]) {
+                System.out.print(temp + " ");
+                total += temp;
             }
+            double rata = total / temps[i].length;
+            System.out.println("\nRata-rata: " + rata);
             System.out.println();
         }
     }
